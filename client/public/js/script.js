f22151e91f69f8c6288a68deae11972bf4e80008
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-01-10 13:38:23 -0600

    images coming back from cloud and displaying on frontend

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -2,18 +2,36 @@ const uploadedImage = document.getElementById('uploadedImg')
 const image = uploadedImage.files[0]
 const uploadFrom = document.getElementById('uploadForm')
 
-// const viewImages = async (e) => {
-//     try {
-//         const response = await fetch('http://localhost:8080/api/download', {
-//             method: 'GET'
-//         })
-//         console.log(await response.json())
+const viewImages = async (e) => {
+    e.preventDefault()
+    try {
+        const response = await fetch('http://localhost:8080/api/download', {
+            method: 'GET'
+        })
+
+        const images = await response.json()
+        console.log(images)
+
+        images.forEach(image => {
+            console.log(image)
+            const matches = image.metadata.mediaLink.match(/\/o\/(.+)\?/)
+            const imageName = matches[1]
+
+            const img = document.createElement('img')
+            img.src = `https://storage.googleapis.com/${image.bucket.name}/${imageName}`
+            img.alt = image.id
+
+            const imgContainer = document.getElementById('imgContainer')
+
+            imgContainer.append(img)
+            
+        })
         
-//     } catch (error) {
-//         console.log(error)
-//         throw new Error(error.message)
-//     }
-// }
+    } catch (error) {
+        console.log(error)
+        throw new Error(error.message)
+    }
+}
 
 
-// document.addEventListener('load', viewImages)
+window.addEventListener('load', viewImages)
--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -1,14 +1,16 @@
 const { Storage } = require('@google-cloud/storage')
 const storage = new Storage()
-const bucket = storage.bucket('shopify-img-repo')
+const bucket = storage.bucket('shopify-image-repo')
 
 const downloadImages = (req, res) => {
+    
     bucket.getFiles((err, files) => {
         if (!err) {
+            console.log(files)
             res.status(200).json(files)
         } else {
             console.log(err)
-            res.status(500).json(err)
+            res.status(res.statusCode).json(err)
             throw new Error('Error getting files')
         }
     })
