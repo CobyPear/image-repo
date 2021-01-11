9feff58f1d8167dc4aa240e083ad5acfdeecddd0
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-01-11 13:25:38 -0600

    fixed preinstall util

--- preinstall.js
+++ preinstall.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
 require('dotenv').config()
 
-const preinstall = () => {
-    fs.writeFile('./google-credentials-heroku.json'), process.env.GOOGLE_CONFIG, (err) => err ? console.log(err) : console.log('key file built successfully!')
-}
+module.exports = fs.writeFile('./google-credentials-heroku.json', process.env.GOOGLE_CONFIG, (err) => err ? console.log(err) : console.log('key file built successfully!'))
 
-module.exports = preinstall
