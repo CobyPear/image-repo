e3d915fef23f4fea328ab2d5e25489d889eba889
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-01-12 15:41:13 -0600

    removed console.logs from script.js

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -67,10 +67,10 @@ const viewImages = async() => {
 // from google client library docs - Google sign-in for Websites
 function onSignIn(googleUser) {
     profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     // show images when user is signed in
     viewImages()
